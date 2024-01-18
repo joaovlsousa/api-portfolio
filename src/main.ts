af20140056100e7052449f078f235dce6f104655
João Vitor <joaov.lmsousa@gmail.com>
2024-01-18 19:45:27 -0300

    feat: add cors

--- src/main.ts
+++ src/main.ts
@@ -4,6 +4,10 @@ import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  app.enableCors({
+    origin: ['http://localhost:3000', env.WEBSITE_URL],
+    methods: ['GET', 'POST'],
+  });
   await app.listen(env.PORT ?? 3333);
 }
 bootstrap();
