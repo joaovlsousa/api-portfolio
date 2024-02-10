e1949078893452c112915c246fea3680dc9bf154
João Vitor <joaov.lmsousa@gmail.com>
2024-02-10 18:51:52 -0300

    fix: fix cors origin

--- src/main.ts
+++ src/main.ts
@@ -4,9 +4,7 @@ import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  app.enableCors({
-    origin: ['https://uploadfy.vercel.app', 'https://joaov.vercel.app'],
-  });
+  app.enableCors();
   await app.listen(env.PORT ?? 3333);
 }
 bootstrap();
