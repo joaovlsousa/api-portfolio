85abc18c49bdc0bedb88b3081b64a08eaf611b6a
João Vitor <joaov.lmsousa@gmail.com>
2024-02-10 19:04:59 -0300

    fix: disabled access origin for file upload

--- src/main.ts
+++ src/main.ts
@@ -3,8 +3,7 @@ import { env } from 'node:process';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
-  app.enableCors();
+  const app = await NestFactory.create(AppModule, { cors: true });
   await app.listen(env.PORT ?? 3333);
 }
 bootstrap();
